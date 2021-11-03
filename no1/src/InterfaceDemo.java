68a58509d1f708708813964466d6706883b90b14
Redstone Zhao <13740080@qq.com>
2021-11-03 14:24:55 +0800

    增加interface 接口的样例代码

---
+++ no1/src/InterfaceDemo.java
@@ -0,0 +1,67 @@
+import static java.lang.System.*;
+
+public class InterfaceDemo {
+    public static void main(String[] args) {
+        SataHdd sh1 = new SeagateHdd(); //初始化希捷硬盘
+        SataHdd sh2 = new SamsungHdd(); //初始化三星硬盘
+
+        out.printf("sh1.readData: %s\n", sh1.readData());
+        out.printf("sh2.readData: %s\n", sh2.readData());
+        sh1.writeData("hello");
+        sh2.writeData("world");
+    }
+}
+
+//串行硬盘接口
+interface SataHdd {
+    //连接线的数量
+    public static final int CONNECT_LINE=4;
+    //写数据
+    public void writeData(String data);
+    //读数据
+    public String readData();
+}
+
+// 维修硬盘接口
+interface fixHdd {
+    // 维修地址
+    String address = "北京市海淀区";
+    // 开始维修
+    boolean doFix();
+}
+
+//希捷硬盘
+class SeagateHdd implements SataHdd, fixHdd {
+    //希捷硬盘读取数据
+    public String readData() {
+        return "from 希捷读取到的数据";
+    }
+    //希捷硬盘写入数据
+    public void writeData(String data) {
+        out.printf("希捷写入成功: %s\n", data);
+    }
+    // 维修希捷硬盘
+    public boolean doFix() {
+        return true;
+    }
+}
+
+//三星硬盘
+class SamsungHdd implements SataHdd {
+    //三星硬盘读取数据
+    public String readData() {
+        return "from 三星读到的数据";
+    }
+    //三星硬盘写入数据
+    public void writeData(String data) {
+        out.printf("三星写入成功: %s\n", data);
+    }
+}
+
+//某劣质硬盘，不能写数据
+abstract class XXHdd implements SataHdd {
+    //硬盘读取数据
+    public String readData() {
+        return "from 劣质硬盘读到数据";
+    }
+}
