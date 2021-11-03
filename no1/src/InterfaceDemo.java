104179866db0b349f648f9f2795577159205866c
Redstone Zhao <13740080@qq.com>
2021-11-03 14:28:37 +0800

    增加抽象类的样例代码

--- no1/src/InterfaceDemo.java
+++ no1/src/InterfaceDemo.java
@@ -4,11 +4,14 @@ public class InterfaceDemo {
     public static void main(String[] args) {
         SataHdd sh1 = new SeagateHdd(); //初始化希捷硬盘
         SataHdd sh2 = new SamsungHdd(); //初始化三星硬盘
+        SataHdd sh3 = new Ai(); // 初始化AI硬盘
 
         out.printf("sh1.readData: %s\n", sh1.readData());
         out.printf("sh2.readData: %s\n", sh2.readData());
+        out.printf("sh3.readData: %s\n", sh3.readData());
         sh1.writeData("hello");
         sh2.writeData("world");
+        sh3.writeData("hahaha");
     }
 }
 
@@ -63,5 +66,12 @@ abstract class XXHdd implements SataHdd {
     //硬盘读取数据
     public String readData() {
         return "from 劣质硬盘读到数据";
+    }
+}
+
+// Ai 硬盘
+class Ai extends XXHdd {
+    public void writeData(String data) {
+        out.println("无法写入，写入出错");
     }
 }
